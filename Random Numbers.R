7c41f3504b838acf22ca39c3314f143e12b623f0
MalualJames <laatmatur9@gmail.com>
2025-10-24 22:59:42 +0300

    Add files via upload

---
+++ Random Numbers.R
@@ -0,0 +1,19 @@
+
+##Random Numbers##
+*****************
+set.seed(123)
+RNgen<-sample(100:200,50, replace=T) 
+RNgen
+max(RNgen)
+min(RNgen)
+sum(RNgen)
+mean(RNgen)
+var(RNgen)
+sd(RNgen)
+hist(RNgen)
+plot(RNgen)
+boxplot(RNgen)
+stem(RNgen)
+summary(RNgen)
+
+
